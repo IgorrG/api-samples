dc5ca5ef581fffb96786c0ccdcea74fbcb501116
Nickolay Shmyrev <nshmyrev@gmail.com>
2019-10-02 22:21:23 +0200

    Added Russian example

---
+++ asterisk/README-RU.md
@@ -0,0 +1,41 @@
+== Использование сервера распознавания с Asterisk
+
+
+=== Устанавливаем зависимости
+
+ - asterisk
+
+ - docker
+
+ - пакеты для работы со звуком: 
+
+        sudo apt install sox espeak
+
+ - пакет для python AGI и вебсокетов
+
+        sudo pip3 install pyst2 websocket
+
+=== Запускаем сервер распознавания
+
+docker run -d -p 2700:2700 alphacep/kaldi-ru:latest
+
+=== Проверяем, что можем запустить EAGI скрипт
+
+```
+python3 eagi-ru.py
+ARGS: ['eagi-ru.py']
+^C
+```
+
+=== Настраиваем план звонков
+
+В etc/extensions.conf
+
+```
+exten => 200,1,Answer()
+same = n,EAGI(/home/user/api-samples/asterisk/eagi-ru.py)
+same = n,Hangup()
+```
+
+=== Звоним и проверяем работу
+
--- asterisk/eagi-ru.py
+++ asterisk/eagi-ru.py
@@ -4,22 +4,26 @@ from asterisk.agi import *
 import os
 from websocket import create_connection
 import json
+import uuid
 
 AUDIO_FD = 3
 CONTENT_TYPE = 'audio/l16; rate=8000; channels=1'
 ACCEPT = 'audio/pcm'
 
+def play_text(agi, text):
+    fn = str(uuid.uuid4())
+    os.system("espeak -v ru -w /tmp/%s.22.wav \"%s\"" % (fn, text))
+    os.system("sox /tmp/%s.22.wav -r 8000 /tmp/%s.wav" % (fn, fn))
+    agi.stream_file("/tmp/%s" % (fn))
+    os.remove("/tmp/%s.22.wav" % (fn)) 
+    os.remove("/tmp/%s.wav" % (fn)) 
 
 def process_chunk(agi, ws, buf):
     ws.send_binary(buf)
     res = json.loads(ws.recv())
-    if 'result' in res:
-        text = " ".join([w['word'] for w in res['result']])
-        agi.verbose(text)
-        os.system("espeak -w /tmp/response22.wav \"" + text.encode('utf-8') + "\"")
-        os.system("sox /tmp/response22.wav -r 8000 /tmp/response.wav")
-        agi.stream_file("/tmp/response")
-        os.remove("/tmp/response.wav")
+    agi.verbose(str(res))
+    if 'text' in res:
+        play_text(agi, "Распознано " + res['text'])
 
 def startAGI():
     agi = AGI()
@@ -27,8 +31,9 @@ def startAGI():
     ani = agi.env['agi_callerid']
     did = agi.env['agi_extension']
     agi.verbose("Call answered from: %s to %s" % (ani, did))
+    play_text(agi, "Привет")
 
-    ws = create_connection("wss://api.alphacephei.com/asr/en")
+    ws = create_connection("ws://localhost:2700")
     try:
         while True:
             data = os.read(AUDIO_FD, 8000)
